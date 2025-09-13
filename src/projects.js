3682badb3611a6e4b9773516b4ff942f9c2aa3ce
sayaka <sayaka@gazzler.com>
2025-09-13 23:29:35 +0200

    feat: projects.js を追加 / Add projects.js to store project data

---
+++ src/projects.js
@@ -0,0 +1,51 @@
+export const projects = [
+  {
+    id: 1,
+    name: "Oha",
+    image: "images/img_oha-min.png",
+    langues: ["HTML", "CSS", "Javascript", "Swiper"],
+    description:
+      "Site statique réalisé pour apprendre les bases du codage, en mettant l’accent sur l’utilisation de CSS (comme Flexbox et positionnement des éléments) et de JavaScript pour ajouter des interactions simples, telles que le menu hamburger et un accordéon pour les questions fréquentes.",
+    tools: ["Figma"],
+    url: "https://oha.sayakatsuji.com",
+  },
+  {
+    id: 2,
+    name: "Snap Onomichi",
+    image: "images/img_onomichi-min.png",
+    langues: ["HTML", "CSS", "Javascript", "Swiper"],
+    description:
+      "Site statique conçu sous forme de projet fictif, notamment pour pratiquer la gestion d’images décoratives adaptées à différentes tailles d'écran. J’ai également intégré des interactions dynamiques à l’aide de JavaScript, telles que deux carrousels d’images et la vérification des champs obligatoires du formulaire de contact avec affichage de messages d'erreur en cas de saisie incorrecte.",
+    tools: ["Figma"],
+    url: "https://onomichi.sayakatsuji.com",
+  },
+  {
+    id: 3,
+    name: "Sobolon",
+    image: "images/img_sobolon-min.png",
+    langues: ["HTML", "CSS", "Javascript", "Swiper"],
+    description:
+      "Site statique conçu pour pratiquer la mise en page et le développement responsive, sous forme de projet fictif. J’ai également intégré des interactions dynamiques à l’aide de JavaScript, telles que des animations d’apparition progressive au défilement et un carrousel d'images.",
+    tools: ["Figma"],
+    url: "https://sobolon.sayakatsuji.com",
+  },
+  {
+    id: 4,
+    name: "Open Cafe",
+    image: "images/img_open-cafe-min.png",
+    langues: ["HTML", "CSS", "Javascript", "PHP", "Swiper"],
+    description:
+      "Site dynamique réalisé avec WordPress dans le cadre de mon apprentissage, visant à explorer les fonctionnalités interactives d’un site web. Ce projet m’a permis de mettre en pratique la gestion de contenu, l’intégration de plugins et la personnalisation des pages en fonction des besoins des utilisateurs.",
+    tools: ["Wordpress", "Figma"],
+    url: "https://opencafe.sayakatsuji.com/",
+  },
+  {
+    id: 5,
+    name: "Portfolio de Sayaka TSUJI",
+    image: "images/img_portfolio-min.png",
+    langues: ["HTML", "CSS", "Javascript"],
+    description:
+      "Poerfolio de Sayaka TSUJI est mon site de portfolio où vous etes actuellement. Il est conçu une approche de design responsive. C'est mon premier site dont je me suis chargée du design.",
+    tools: ["Vite", "Figma"],
+  },
+];
